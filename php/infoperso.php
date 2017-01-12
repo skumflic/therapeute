6403210587bdf97927e61903fdba80d03aaf5141
skumflic <corentin_25@hotmail.fr>
2017-01-12 21:10:13 +0100

    Add files via upload

--- php/infoperso.php
+++ php/infoperso.php
@@ -238,11 +238,15 @@
 	}
 	
 	
+	
 	if(isset($_POST['btnValiderPhoto'])) {
 		$target_dir = "../upload/";
 		$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 		$uploadOk = 1;
 		$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+		
+		$imageName = $id. '.' . $imageFileType;
+		$target_file = $target_dir.$imageName;
 		// Check if image file is a actual image or fake image
 		if(isset($_POST["submit"])) {
 			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -254,15 +258,7 @@
 				$uploadOk = 0;
 			}
 		}
-		// Check if file already exists
-		if (file_exists($target_file)) {
-			$S = "UPDATE THERAPEUTE 
-					SET lienPhoto = '". basename( $_FILES["fileToUpload"]["name"]). "'
-					WHERE id = '$id'";
-			
-			mysqli_query($bd, $S) or gk_cb_bd_erreur($bd, $S);
-			
-		}
+		
 		// Check file size
 		if ($_FILES["fileToUpload"]["size"] > 500000) {
 			echo "Sorry, your file is too large.";
@@ -274,9 +270,12 @@
 		// if everything is ok, try to upload file
 		} else {
 		    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-			echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+			echo "The file ". $imageName. " has been uploaded.";
 			
-			
+			$S = "UPDATE THERAPEUTE 
+					SET lienPhoto = '". $imageName . "'
+					WHERE id = '$id'";
+			mysqli_query($bd, $S) or gk_cb_bd_erreur($bd, $S);		
 			
 		    } else {
 			echo "Sorry, there was an error uploading your file.";
@@ -288,6 +287,7 @@
 		
 	}
 	
+			
 	function changePass ($ancienPass) {
 		
 		
