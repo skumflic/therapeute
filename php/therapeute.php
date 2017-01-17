9f0c2fdad932dd1b5e688833c12fe96624c8ed41
skumflic <corentin_25@hotmail.fr>
2017-01-17 21:49:00 +0100

    Add files via upload

--- php/therapeute.php
+++ php/therapeute.php
@@ -65,10 +65,17 @@
 	$sql_reseau="SELECT * 
 			FROM THERA_RESEAU
 			WHERE idTherapeute = '$profil_id'";
-	$r_reseau = mysqli_query($bd, $sql_reseau) or gk_bd_erreur($bd, $sql_tarif);
+	$r_reseau = mysqli_query($bd, $sql_reseau) or gk_bd_erreur($bd, $sql_reseau);
 	
 	
 	
+	//CABINET
+	$sql_cabinet="SELECT *
+			FROM PHOTO
+			WHERE idTherapeute = '$profil_id'
+			AND afficher = true";
+	$r_cabinet = mysqli_query($bd, $sql_cabinet) or gk_bd_erreur($bd, $sql_cabinet);
+
 	
 	
 	
@@ -90,7 +97,7 @@
 	if ($skin == 1) 
 		afficher_alpha($color_to_string);
 	if ($skin == 2) 
-		afficher_alpha($color_to_string);
+		afficher_miniport($color_to_string);
 		//~ afficher_miniport($color_to_string);
 	if ($skin == 3) 
 		afficher_prologue($color_to_string);
@@ -101,7 +108,7 @@
 	
 	
 	function afficher_alpha($color) {
-		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail;
+		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail, $r_cabinet;
 		
 		$dir_to_css_color = "../style/color/$color.css";
 		
@@ -112,7 +119,7 @@
 		html_alpha_section_banner($nom, $prenom, $titre, $description);
 		html_alpha_main($lienPhoto, $aboutme, $remerciements, $r_formation);
 		html_alpha_two();
-		html_alpha_three($r_tarif);
+		html_alpha_three($r_tarif, $r_cabinet);
 		html_alpha_cta();	
 		
 		html_alpha_footer($r_reseau, $mail);
@@ -121,7 +128,7 @@
 	}
 	
 	function afficher_miniport($color) {
-		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail;
+		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail, $r_cabinet;
 	
 		$dir_to_css_color = "$color.css";
 		html_miniport_start($title, $dir_to_css_color);
@@ -132,6 +139,12 @@
 		html_miniport_remerciements($remerciements); 
 		if (mysqli_num_rows($r_formation) > 0) 
 			html_miniport_formation($r_formation);
+		html_miniport_methode();
+		html_miniport_delmet();
+		html_miniport_deontologie();
+		html_miniport_certification();
+		if (mysqli_num_rows($r_cabinet) > 0) 
+			html_miniport_cabinet($r_cabinet);	
 		html_miniport_tarif($r_tarif);
 		html_miniport_contact($r_reseau, $mail);
 		
@@ -140,7 +153,7 @@
 	}
 	
 	function afficher_prologue($color) {
-		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail;
+		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail, $r_cabinet;
 	
 		$dir_to_css_color = "../style/color/$color.css";	
 		html_prologue_start($title, $dir_to_css_color);
@@ -150,7 +163,7 @@
 		html_prologue_intro($nom, $prenom, $titre, $description);
 		html_prologue_about($remerciements, $aboutme, $r_formation);
 		html_prologue_methode();
-		html_prologue_cabinet($r_tarif);
+		html_prologue_cabinet($r_tarif, $r_cabinet);
 		html_prologue_contact();
 		
 		html_prologue_end_main();
@@ -160,7 +173,7 @@
 	}
 	
 	function afficher_readonly($color) {
-		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail;
+		global $title, $titre, $description, $nom, $prenom, $lienPhoto, $remerciements, $aboutme, $r_formation, $r_experience, $r_tarif, $r_reseau, $mail, $r_cabinet;
 	
 		$dir_to_css_color = "../style/color/$color.css";	
 		html_readonly_start($title, $dir_to_css_color);
@@ -169,7 +182,7 @@
 		
 		html_readonly_one($nom, $prenom, $aboutme, $remerciements, $r_formation);
 		html_readonly_two();
-		html_readonly_three($r_tarif);
+		html_readonly_three($r_tarif, $r_cabinet);
 		
 		
 		html_readonly_end_main();
